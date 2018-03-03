6d03bb538fa90e20d1d2acfc834b19f2d81ac7b8
ab <a@b.com>
2018-03-03 20:08:29 +0100

    naprawa ale nie do końca

---
+++ zad_2-3.R
@@ -0,0 +1,13 @@
+years <- c(1800:2018)
+
+for (i in years) {
+  if(i %% 4 == 0) {
+    if(i %% 100 != 0) {
+      print(i)
+    }
+  } else if(i %% 400 == 0) {
+    print(i)
+  } else {
+    i <- i + 1
+  }
+}
--- zad_2-5.R
+++ zad_2-5.R
@@ -1,8 +1,9 @@
 fibonacci <- function(n, a = 0, b = 1) {
-  if(n == 1)
+  if(n == 1) {
     return(0)
-  else if(n ==2)
+  } else if(n ==2) {
     return(b)
-  else
+  } else {
     return(fibonacci(n - 1, b, a + b))
+  }
 }
