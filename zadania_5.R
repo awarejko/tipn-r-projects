8d1dd3362810ac470d8159f89e8c94e6832f02b4
ab <a@b.com>
2018-06-11 16:28:29 +0200

    poprawione zadanie 5

--- zadania_5.R
+++ zadania_5.R
@@ -10,7 +10,7 @@ View(movies)
 
 # Zad_5-2
 
-filter(movies, year == 2005)
+filter(movies, Comedy == 1)
 
 # Zad_5-3
 
@@ -18,8 +18,26 @@ movies %>% select(title, year, budget) %>% arrange(desc(budget))
 
 # Zad_5-4
 
-movies %>% select(Animation, year) %>% filter(year == 1990)
+anim <- movies %>% select(title, Animation, year, rating) %>% 
+  filter(year == 1990:1999) %>% filter(Animation == 1) %>% arrange(desc(rating))
+View(anim)
 
 # Zad_5-5
 
-movies %>% select(Drama, length) %>% arrange(desc(length))
+dramas <- movies %>% select(title, Drama, length) %>% filter(Drama ==1) %>% arrange(desc(length))
+View(dramas)
+
+# Zad_5-6
+
+mpaa_R <- movies %>% select(mpaa, rating) %>% filter(mpaa == "R")
+rsum <- summarise(mpaa_R, rating_mean = mean(rating, na.rm = T),
+                      rating_sd = sd(rating, na.rm = T))
+View(rsum)
+mpaa_13 <- movies %>% select(mpaa, rating) %>% filter(mpaa == "PG-13")
+pg13sum <- summarise(mpaa_13, rating_mean = mean(rating, na.rm = T),
+                     rating_sd = sd(rating, na.rm = T))
+View(pg13sum)
+mpaa_pg <- movies %>% select(mpaa, rating) %>% filter(mpaa == "PG")
+pgsum <- summarise(mpaa_pg, rating_mean = mean(rating, na.rm = T),
+                   rating_sd = sd(rating, na.rm = T))
+View(pgsum)
